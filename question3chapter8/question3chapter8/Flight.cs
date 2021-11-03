380c0bfadc7777e7dc96c7b61cd54df71a9782a1
Douglas Masho <douglasmasho@gmail.com>
2021-11-03 16:32:53 +0200

    added question 3

---
+++ question3chapter8/question3chapter8/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace question3chapter8
+{
+    class Flight
+    {
+        public int FlightNum { get; set; }
+        public string AirportCode { get; set; }
+        public string AirportName { get; set; }
+        public int Time { get; set; }
+        public Flight(int flightNum, string airportCode, string airportName, int time)
+        {
+            this.FlightNum = flightNum;
+            this.AirportCode = airportCode;
+            this.AirportName = airportName;
+            this.Time = time;
+        }
+    }
+}
---
+++ question3chapter8/question3chapter8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace question3chapter8
+{
+    class Program
+    {
+        public Flight[] FlightArr { get; set; }
+        public Program()
+        {
+            Flight Flight1 = new Flight(201, "AUS", "Austin", 0710);
+            Flight Flight2 = new Flight(321, "CRP", "Corpus Christi", 0830);
+            Flight Flight3 = new Flight(510, "DFW", "Dallas Fort Worth", 0915);
+            Flight Flight4 = new Flight(633, "HOU", "Houston", 1140);
+            Flight[] flightArr = { Flight1, Flight2, Flight3, Flight4 };
+            this.FlightArr = flightArr;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            Console.WriteLine(program.GetFlightInfo(21));
+        }
+
+        public string GetFlightInfo(int flightNum)
+        {
+            string result = "Could not find your flight";
+            //look for the right flight 
+            foreach(Flight flight in FlightArr)
+            {
+                if(flight.FlightNum == flightNum)
+                {
+                    result = $"Airport Code: {flight.AirportCode} \n Airport Name: {flight.AirportName} \n Flight Time: {flight.Time}";
+                }
+            }
+            return result;
+        }
+    }
+}
