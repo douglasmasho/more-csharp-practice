6f3fcd1f44cbbb35345c9b59ff0c6cc1b6b278dc
Douglas Masho <douglasmasho@gmail.com>
2021-11-04 12:23:18 +0200

    tested question 8

---
+++ question8chapter8/question8chapter8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+//dont forget to import these
+using System.Collections.Generic;
+using System.Linq;
+/*
+Write a program named SortWords that includes a method that accepts any number 
+of words and sorts them in alphabetical order. Demonstrate that the program works 
+correctly when the method is called with one, two, five, and ten words. 
+*/
+
+
+namespace question8chapter8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SortWords("Oh");
+            Console.WriteLine("..........................");
+            SortWords("Oh", "yeah");
+            Console.WriteLine("..........................");
+            SortWords("Oh", "yeah", "ay", "Halla", "Boyz");
+            Console.WriteLine("..........................");
+            SortWords("Oh", "yeah", "ay", "Halla", "Boyz", "Halla", "We", "Dem", "Boys", "Holla");
+        }
+
+        public static void SortWords(params string[] words)
+        {
+            List<string> wordList = words.ToList();
+
+            wordList.Sort();
+
+            string[] wordArr = wordList.ToArray();
+
+            foreach(string word in wordArr)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
