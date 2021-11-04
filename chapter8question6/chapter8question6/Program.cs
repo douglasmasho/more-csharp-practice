51debca2244c396284bf1c4784f94e24ebe8fa09
Douglas Masho <douglasmasho@gmail.com>
2021-11-04 11:41:03 +0200

    did question 6

---
+++ chapter8question6/chapter8question6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace chapter8question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second;
+            InputMethod(out first, out second);
+            Console.WriteLine("After InputMethod first is {0}", first);
+            Console.WriteLine("and second is {0}", second);
+        }
+        private static void InputMethod(out int one, out int two)
+        {
+            one = DataEntry("first");
+            two = DataEntry("second");
+        }
+        private static int DataEntry(string position)
+        {
+            Console.WriteLine($"Enter {position} integer");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
